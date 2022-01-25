c3d35345fbac5ababeaba57bb3ea8d4f19b70ea0
dork <38471874+qt-dork@users.noreply.github.com>
2022-01-25 00:35:16 -0800

    i think this works omg this is so dumb but it works

--- components/ImageWrapper.js
+++ components/ImageWrapper.js
@@ -1,20 +1,13 @@
-import { useCallback, useRef, useState, useEffect } from "react";
-import { useClipboard } from "use-clipboard-copy";
-import { toPng, toBlob } from "html-to-image";
+import { useRef } from "react";
 import styled from "styled-components";
 
-import { useIsMobileOrTablet } from "../hooks/isMobileOrTablet";
 import WordleGraph from "./WordleGraph";
+import ShareButton from "./ShareButton"
 
 const OutputContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
   flex-grow: 1;
-`
-
-const StyledButton = styled.button`
-  height: 3rem;
-  margin-top: 0.3rem;
 `
 
 const Output = styled.div`
@@ -28,51 +21,7 @@ const Wrapper = styled.div`
 `
 
 const ImageWrapper = ({grid, firstLine, output}) => {
-  const istMobOrTab = useIsMobileOrTablet();
   const wordleRef = useRef(null);
-  const clipboard = useClipboard();  
-
-  const onButtonClick = useCallback(() => {
-    if (wordleRef.current === null) {
-      return
-    }
-
-    if (navigator.canShare && istMobOrTab) {
-      toBlob(wordleRef.current, {cacheBust:true, backgroundColor: "#ffffff"})
-      .then((blob) => {
-        const filesArray = [
-          new File(
-            [blob],
-            'wordle-image.png',
-            {
-              type: "image/png",
-              lastModified: new Date().getTime()
-            }
-          )
-        ];
-        const shareData = {
-          files: filesArray,
-        };
-
-        navigator.share(shareData);
-      })
-      .catch((err) => {
-        console.error('oops, something went wrong!', err);
-      });
-    }
-    else {
-      toPng(wordleRef.current, { cacheBust: true, backgroundColor: "#ffffff" })
-      .then((dataUrl) => {
-        const link = document.createElement('a')
-        link.download = `wordle-image.png`
-        link.href = dataUrl
-        link.click()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-  }, [wordleRef]);
 
   return (
     <>
@@ -81,7 +30,7 @@ const ImageWrapper = ({grid, firstLine, output}) => {
           <label htmlFor="output">Shareable:</label>
           <Output><WordleGraph grid={grid} firstLine={firstLine} childRef={wordleRef} /></Output>
         </Wrapper>
-        <StyledButton onClick={() => {clipboard.copy(output); onButtonClick()}}>Save Image and Copy Alt-Text</StyledButton>
+        <ShareButton output={output} wordleRef={wordleRef}>Save Image and Copy Alt-Text</ShareButton>
       </OutputContainer>
     </>
   );
