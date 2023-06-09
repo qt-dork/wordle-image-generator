6fef3b9b6b81198f40fcc08a4088565f64a29c9f
dork <38471874+qt-dork@users.noreply.github.com>
2023-06-09 00:01:37 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -91,7 +91,7 @@
   </div>
   <footer id="credit">
     <p>
-      <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> &copy; <a href="https://www.powerlanguage.co.uk/">Josh Wardle</a> (<a href="https://twitter.com/powerlanguish">@powerlanguish</a>). Alt-text (and most other) code heavily borrowed from <a href="https://wa11y.co/">wa11y.co</a> by <a href="https://cariad.earth/">Cariad Eccleston</a> (<a href="https://twitter.com/antagonistapp/">@antagonistapp</a>). Wordle Image Generator by Evie Finch (<a href="https://twitter.com/qt_dork/">@qt_dork</a>) Source and bug reports: <a href="https://github.com/qt-dork/wordle-image-generator">qt-dork/wordle-image-generator</a>.
+      <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> &copy; <a href="https://www.powerlanguage.co.uk/">Josh Wardle</a> (<a href="https://twitter.com/powerlanguish">@powerlanguish</a>). Alt-text code borrowed from <a href="https://wa11y.co/">wa11y.co</a> by <a href="https://cariad.coffee/">Cariad Eccleston</a> (<a href="https://tech.lgbt/@cariad">@cariad@tech.lgbt</a>). Wordle Image Generator by Evie Finch (<a href="https://cohost.org/ewie/">@ewie</a>) Source and bug reports: <a href="https://github.com/qt-dork/wordle-image-generator">qt-dork/wordle-image-generator</a>.
     </p>
   </footer>
 
